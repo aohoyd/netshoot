ea734f8ba07228d62976820114dda5dd5a071b29
Alexey Olshanskiy <gh@aohoy.dev>
2026-01-16 17:19:08 +0300

    update packages

--- packages/config.fish
+++ packages/config.fish
@@ -11,6 +11,15 @@ function fish_greeting
     echo ""
 end
 
+function apk --wraps="/sbin/apk"
+    # Fix apk db because of corrupted @local
+	for package in (sed -n '/@local/{s/=.*//;p}' /etc/apk/world)
+        sed -i "/^P:$package\$/,/^\$/d" /lib/apk/db/installed
+    end
+    sed -i '/@local/d' /etc/apk/world
+	/sbin/apk $argv
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
